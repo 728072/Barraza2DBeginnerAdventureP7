67659798007b4c5aeb5e0ddc419561a3b297fa43
728072 <728072@student.dusd.net>
2025-11-03 15:24:41 -0800

    Added working movement and expanded map

--- P7BarrazaAdrian2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
+++ P7BarrazaAdrian2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
@@ -1,40 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public InputAction MoveAction;
+    public float speed = 1.5f;
+
     void Start()
     {
-        
+
+        //QualitySettings.vSyncCount = 0;
+        //Application.targetFrameRate = 10;
+
+        MoveAction.Enable();
     }
 
-    // Update is called once per frame
     void Update()
     {
 
-        float horizontal = 0.0f;
-
-        if (Keyboard.current.leftArrowKey.isPressed)
-        {
-
-            horizontal = -1.0f;
-
-        }
-
-        else if (Keyboard.current.rightArrowKey.isPressed)
-        {
-
-            horizontal = 1.0f;
-
-        }
-
-        Vector2 position = transform.position;
-
-        position.x = position.x + 0.05f;
-        position.y = position.y + 0.05f;
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position + move * speed * Time.deltaTime;
         transform.position = position;
     }
 }
